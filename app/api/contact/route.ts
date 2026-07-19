c6e54f9387a3d5b24fa25ad50c56d010b68d5ec2
Kavita <kannuarmy2@gmail.com>
2026-07-19 17:05:03 +0530

    changes email and pass

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,6 +4,8 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
     const { name, email, subject, message } = body;
+    console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
     const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
